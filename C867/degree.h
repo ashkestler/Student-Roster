81d57898861c5d9d8e61e138cff1841f126c1f33
Ash Kestler <ashleykestler@gmail.com>
2022-06-18 15:48:28 -0400

    File cleanup

--- C867/degree.h
+++
@@ -1,9 +0,0 @@
-#ifndef DEGREE_H
-#define DEGREE_H
-#include <string>
-using namespace std;
-
-//Define enumerated data type
-enum class DegreeProgram { SECURITY, NETWORK, SOFTWARE, INVALID };
-
-#endif
